6023f24961179db811692df2cc8f683acdf493d6
Lara Rodrigues <lararods73@gmail.com>
2025-01-17 13:31:50 -0300

    adding back-end connection(mocked)

--- src/components/email-form/email-form.tsx
+++ src/components/email-form/email-form.tsx
@@ -2,13 +2,16 @@ import { useState } from 'react';
 import Button from '../button/button'
 import './email-form.css'
 import { validateEmail } from '../../utils/email-validation';
+import { toast } from 'react-toastify';
 
 interface EmailFormProps {
-    onSubmit: (email: string) => void; 
+    onSubmit: (email: string) => void;
 }
 function EmailForm({ onSubmit }: EmailFormProps) {
     const [email, setEmail] = useState<string>('');
     const [isValid, setIsValid] = useState<boolean>(true);
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value;
@@ -16,9 +19,27 @@ function EmailForm({ onSubmit }: EmailFormProps) {
         setIsValid(validateEmail(value));
     };
 
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
         if (isValid && email) {
-        onSubmit(email); 
+            setIsSubmitting(true);
+            try {
+                const response = await fetch('http://localhost:3000/subscribe', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ email }),
+                });
+
+                if (response.ok) {
+                    onSubmit(email);
+                }
+            } catch (error) {
+                toast.error("Error subscribing, try again")
+                console.error('Error during subscription:', error);
+            } finally {
+                setIsSubmitting(false); 
+            }
         }
     };
 
@@ -29,7 +50,8 @@ function EmailForm({ onSubmit }: EmailFormProps) {
                 {!isValid && (<small>Please enter a valid Email address</small>)}
                 <input value={email}
                     onChange={handleChange} placeholder='email@company.com' type="email" id="email" name="email" />
-                <Button text="Subscribe to monthly newsletter" onClick={handleSubmit}></Button>
+                <Button text={isSubmitting ? 'Subscribing...' : 'Subscribe to monthly newsletter'}
+                    onClick={handleSubmit}></Button>
             </div>
         </>
     )
